5b51fb76d5c94f74f402442f3b796003d9ce69c9
Bieber0125 <13750226@me.mcu.edu.tw>
2025-09-11 17:36:04 +0800

    week01

---
+++ week01/week01-1.cpp
@@ -0,0 +1,14 @@
+// ¦b°®½_¯ó°ï¸Ì§ä°w
+//haystack: sadbutsad
+//needle: sad
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int H = haystack.length(), N = needle.length();
+        for(int i=0; i <= H - N; i++) {
+            if( haystack.substr(i, N) ==needle) return i;
+        }
+
+        return -1;
+    }
+};
