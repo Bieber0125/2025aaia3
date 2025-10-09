4eb7e39d55a5d7bbefd5a0d86add8c23f0d3598c
Bieber0125 <13750226@me.mcu.edu.tw>
2025-10-09 17:08:36 +0800

    week05

---
+++ week05/week05-1.cpp
@@ -0,0 +1,12 @@
+//week05-1.cpp
+//LeetCode ¾Ç²ß­pµe
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        while( ss >> word ){
+        }
+        return word.length();
+    }
+};
