d24f0a368a5e454d0917a56f9314878af3ac5a81
13750226 <13750226@me.mcu.edu.tw>
2025-11-20 16:53:55 +0800

    week11

---
+++ week11/week11-1a.cpp
@@ -0,0 +1,13 @@
+//week11-1a.cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+	char c;
+	while( cin >> c ){
+		if(c>='A' && c<='Z') c = c - 'A' + 'a';
+		else if(c>='a'&&c<='z')c = c - 'a' + 'A';
+		cout << c;
+	}
+	cout << "\n";
+}
